b1480be895878a8df6ed6def91ccb6c156a7187a
Tetiana Protas <protas.tb@gmail.com>
2024-02-28 19:38:53 +0200

    change backend rendering

--- src/api/users.ts
+++ src/api/users.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 import { Answers } from "../utils";
 
 // const baseUrl = "https://64a0823fed3c41bdd7a75666.mockapi.io/users";
-const baseUrl = "https://yogather-backend.vercel.app/";
+// const baseUrl = "https://yogather-backend.vercel.app/";
+const baseUrl = "https://yogather-backend.onrender.com/";
 
 export interface User {
     _id?: any;
